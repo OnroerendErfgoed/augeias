43c9be46fc446e9bea74faa4f06917c602dca29c
Tinne Cahy <tinne.cahy@geosolutions.be>
2015-09-29 11:04:52 +0200

    add includeme

--- augeias/__init__.py
+++ augeias/__init__.py
@@ -2,11 +2,8 @@ from pyramid.config import Configurator
 from pyramid.settings import asbool
 
 
-def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
-    config = Configurator(settings=settings)
-
+def includeme(config):
+    """this function adds some configuration for the application"""
     # Rewrite urls with trailing slash
     config.include('pyramid_rewrite')
     config.add_rewrite_rule(r'/(?P<path>.*)/', r'/%(path)s')
@@ -29,8 +26,16 @@ def main(global_config, **settings):
     config.add_route('get_object', pattern='/collections/{collection_key}/containers/{container_key}/{object_key}',
                      request_method="GET")
 
+    config.scan()
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    config = Configurator(settings=settings)
+
+    includeme(config)
+
     if asbool(settings.get('augeias.init_collections', True)):  # pragma: no cover
         config.include('augeias.collections')
 
-    config.scan()
     return config.make_wsgi_app()
