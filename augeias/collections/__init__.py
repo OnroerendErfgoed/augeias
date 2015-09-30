7d4c9b5a6a92e15de0d907583505141f7e313b47
Tinne Cahy <tinne.cahy@geosolutions.be>
2015-09-30 10:11:34 +0200

    collections list

--- augeias/collections/__init__.py
+++ augeias/collections/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from augeias.stores.PairTreeFileSystemStore import PairTreeFileSystemStore
 from augeias.collections.model import Collection
-
+import os
 
 def includeme(config):   # pragma: no cover
-    beeldbank = Collection(name='beeldbank', object_store=PairTreeFileSystemStore('~/data/beeldbank_store/data'))
+    beeldbank = Collection(name='beeldbank', object_store=PairTreeFileSystemStore(os.path.expanduser('~/data/beeldbank_store/data')))
     config.registry.collections[beeldbank.name] = beeldbank
 
-    besluiten = Collection(name='besluiten', object_store=PairTreeFileSystemStore('~/data/besluiten_store/data'))
+    besluiten = Collection(name='besluiten', object_store=PairTreeFileSystemStore(os.path.expanduser('~/data/besluiten_store/data')))
     config.registry.collections[besluiten.name] = besluiten
 
