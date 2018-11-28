2d08b9bb0ce51db929dd8a2a2f62dca56701f420
Wouter Claeys <wouter.claeys@vlaanderen.be>
2018-11-28 12:49:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requires = [
     ]
 
 setup(name='augeias',
-      version='0.4.1',
+      version='0.5.0',
       description='Augeias. Stores your files.',
       long_description=README + '\n\n' + HISTORY,
       classifiers=[
